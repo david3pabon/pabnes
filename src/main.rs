49a7d3975a288c245d5405cfd90b500fda21f661
David P <david.pabon@gmail.com>
2020-11-24 22:51:57 -0800

    Reading file and puting all bytes in array

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::env;
+use std::fs;
+
 fn main() {
-    println!("Hello, world!");
+    let mut args: Vec<String> = env::args().collect();
+    args.remove(0);
+
+    let filename = match args.pop() {
+        Some(val) => val,
+        None => panic!("No such file"),
+    };
+
+    let data: Vec<u8> = fs::read(&filename).expect("Unable to read file");
+    let byte = *data.get(0).unwrap();
+    assert_eq!(78, byte);
 }
