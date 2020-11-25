06eebc11c1bad64330b9ce6693d99eee86b05330
David P <david.pabon@gmail.com>
2020-11-24 23:29:57 -0800

    Slices!

--- src/main.rs
+++ src/main.rs
@@ -4,13 +4,9 @@ use std::fs;
 fn main() {
     let mut args: Vec<String> = env::args().collect();
     args.remove(0);
-
-    let filename = match args.pop() {
-        Some(val) => val,
-        None => panic!("No such file"),
-    };
+    let filename = args.pop().expect("No file");
 
     let data: Vec<u8> = fs::read(&filename).expect("Unable to read file");
-    let byte = *data.get(0).unwrap();
-    assert_eq!(78, byte);
+    let bytes = &data[0..3];
+    assert_eq!(bytes, "NES".as_bytes());
 }
