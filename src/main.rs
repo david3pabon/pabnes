64d379bc171506f32667d8702df9e45a33b62f84
David P <david.pabon@rover.com>
2021-07-12 19:13:11 -0700

    Static map with operational codes

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,21 @@
 // use std::env;
 // use std::fs;
 
-mod cpu;
+// mod cpu;
+// mod opcodes;
 
-use cpu::CPU;
+// use cpu::CPU;
 
 fn main() {
-    let data: Vec<u8> = vec![0x85, 0x10, 0x00];
+    // let data: Vec<u8> = vec![0x85, 0x10, 0x00];
 
-    let mut cpu = CPU::new();
+    // let mut cpu = CPU::new();
 
-    cpu.load(data);
-    cpu.reset();
-    cpu.reg_a = 0x55;
-    cpu.run();
+    // cpu.load(data);
+    // cpu.reset();
+    // cpu.reg_a = 0x55;
+    // cpu.run();
 
     // println!("{}", cpu.mem_read(0x10));
+    println!("{}", "Hello, World!");
 }
--- src/opcodes.rs
+++ src/opcodes.rs
@@ -1,4 +1,6 @@
-
+use crate::cpu::AddressingMode;
+use std::collections::HashMap;
+use lazy_static::lazy_static;
 
 pub struct OpCode {
     pub code: u8,
@@ -11,25 +13,46 @@ pub struct OpCode {
 impl OpCode {
     fn new(code: u8, mnemonic: &'static str, len: u8, cycles: u8, mode: AddressingMode) -> OpCode {
         OpCode {
-            mode: mode,
+            code: code,
             mnemonic: mnemonic,
             len: len,
             cycles: cycles,
-            mode: mode
+            mode: mode,
         }
     }
 }
 
 lazy_static! {
-    pub static CPU_OPS_CODES = vec![
-        OpCode::new(0x00, "BRK", 1, 7, AddressingMode::NoneAddressing);
-        OpCode::new(0x00, "BRK", 1, 7, AddressingMode::NoneAddressing);
-        OpCode::new(0x00, "BRK", 1, 7, AddressingMode::NoneAddressing);
-        OpCode::new(0x00, "BRK", 1, 7, AddressingMode::NoneAddressing);
-        OpCode::new(0x00, "BRK", 1, 7, AddressingMode::NoneAddressing);
-        OpCode::new(0x00, "BRK", 1, 7, AddressingMode::NoneAddressing);
-        OpCode::new(0x00, "BRK", 1, 7, AddressingMode::NoneAddressing);
-        OpCode::new(0x00, "BRK", 1, 7, AddressingMode::NoneAddressing);
-        OpCode::new(0x00, "BRK", 1, 7, AddressingMode::NoneAddressing);
-    ]
+    pub static ref CPU_OPS_CODES: Vec<OpCode> = vec![
+        OpCode::new(0x00, "BRK", 1, 7, AddressingMode::NoneAddressing),
+        OpCode::new(0xaa, "TAX", 1, 2, AddressingMode::NoneAddressing),
+        OpCode::new(0xe8, "INX", 1, 2, AddressingMode::NoneAddressing),
+
+        OpCode::new(0xa9, "LDA", 2, 2, AddressingMode::Immediate),
+        OpCode::new(0xa5, "LDA", 2, 3, AddressingMode::ZeroPage),
+        OpCode::new(0xb5, "LDA", 2, 4, AddressingMode::ZeroPageX),
+        OpCode::new(0xad, "LDA", 3, 4, AddressingMode::Absolute),
+        OpCode::new(0xbd, "LDA", 3, 4/*+1 if page crossed*/, AddressingMode::AbsoluteX),
+        OpCode::new(0xb9, "LDA", 3, 4/*+1 if page crossed*/, AddressingMode::AbsoluteY),
+        OpCode::new(0xa1, "LDA", 2, 6, AddressingMode::IndirectX),
+        OpCode::new(0xb1, "LDA", 2, 5/*+1 if page crossed*/, AddressingMode::IndirectY),
+
+        OpCode::new(0x85, "STA", 2, 3, AddressingMode::ZeroPage),
+        OpCode::new(0x95, "STA", 2, 4, AddressingMode::ZeroPageX),
+        OpCode::new(0x8d, "STA", 3, 4, AddressingMode::Absolute),
+        OpCode::new(0x9d, "STA", 3, 5, AddressingMode::AbsoluteX),
+        OpCode::new(0x99, "STA", 3, 5, AddressingMode::AbsoluteY),
+        OpCode::new(0x81, "STA", 2, 6, AddressingMode::IndirectX),
+        OpCode::new(0x91, "STA", 2, 6, AddressingMode::IndirectY),
+    ];
+
+    pub static ref OPCODES_MAP: HashMap<u8, &'static OpCode> = {
+        let mut map = HashMap::new();
+        
+        for op_code in &*CPU_OPS_CODES {
+            map.insert(op_code.code, op_code);
+        }
+
+        map
+    };
 }
